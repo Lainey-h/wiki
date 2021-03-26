6ce8cb1218113d5ab7345e04597b687eb001fb66
Lainey-h <houlingling.1@outlook.com>
2021-03-26 15:45:53 +0800

    2-7 增加hello接口测试test.http

--- src/main/java/com/lainey/wiki/controller/TestController.java
+++ src/main/java/com/lainey/wiki/controller/TestController.java
@@ -1,15 +1,35 @@
 package com.lainey.wiki.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController// RestContraller用来返回字符串
 //@Controller//用来返回页面 表示访问地址("/hello")的时候要返回一个页面
 public class TestController {
+
+    @Value("${test.hello:TEST}")
+    private String testHello;
+    /*
+    GET,POST,PUT,DELETE
+    /USER/1
+    @RETURN
+     */
+    //@PostMapping
+    //PutMapping
+    //DeleteMapping
+    //@requestMapping(value="/user/1,method=RequestMethod.GET)
+    //@requestMapping(value="/user/1,method=RequestMethod.DELETE)
+
     //http://127.0.0.1:8880/hello
-    @RequestMapping("/hello")//接口
+    @GetMapping("/hello")//接口
     public String hello(){
-        return "Hello World";
+        return "Hello World!"+testHello;
     }
+    @PostMapping("/hello/post")
+    public String helloPost(String name){
+        return "Hello World! Post,"+ name;
+    }
+
 }
