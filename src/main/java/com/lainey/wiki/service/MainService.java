058b64b8f66bcb5009542b6042b94d63998dc592
Lainey-h <houlingling.1@outlook.com>
2021-04-11 17:16:02 +0800

    3-7-1 封装案例主表main的请求参数和返回参数(补)

--- src/main/java/com/lainey/wiki/service/MainService.java
+++ src/main/java/com/lainey/wiki/service/MainService.java
@@ -1,7 +1,11 @@
 package com.lainey.wiki.service;
 
 import com.lainey.wiki.domain.Main;
+import com.lainey.wiki.domain.MainExample;
 import com.lainey.wiki.mapper.MainMapper;
+import com.lainey.wiki.req.MainReq;
+import com.lainey.wiki.resp.MainResp;
+import com.lainey.wiki.util.CopyUtil;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
@@ -13,7 +17,21 @@ public class MainService {
     @Resource
     private MainMapper mainMapper;
 
-    public List<Main> list(){
-        return mainMapper.selectByExample(null);
+    public List<MainResp> list(MainReq req){
+        MainExample mainExample = new MainExample();
+        MainExample.Criteria criteria=mainExample.createCriteria();
+        criteria.andAlbhEqualTo(req.getAlbh());
+        List<Main> mainList = mainMapper.selectByExample(mainExample);
+
+      /*  List<MainResp> respList = new ArrayList<>();
+        for (Main main : mainList) {
+            MainResp mainResp = new MainResp();
+            BeanUtils.copyProperties(main,mainResp);
+            // mainResp.setBz("备注"); 该句是用来测试是否生效的
+            respList.add(mainResp);
+        }*/
+
+        List<MainResp> list = CopyUtil.copyList(mainList, MainResp.class);
+        return list;
     }
 }
