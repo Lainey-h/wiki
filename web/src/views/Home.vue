ac226987220d7d6cde4c7d75c50a80ddc2ddb52e
Lainey-h <houlingling.1@outlook.com>
2021-04-14 15:43:51 +0800

    5-4 加入Ant Design Vue table组件

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -57,11 +57,56 @@
     <a-layout-content
         :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
     >
-      <pre>
-        {{ebooks}}
-        {{ebooks2}}
-        {{mains}}
-      </pre>
+      <a-table :data-source="data" :columns="columns">
+        <template #filterDropdown="{ setSelectedKeys, selectedKeys, confirm, clearFilters, column }">
+          <div style="padding: 8px">
+            <a-input
+                ref="searchInput"
+                :placeholder="`Search ${column.dataIndex}`"
+                :value="selectedKeys[0]"
+                style="width: 188px; margin-bottom: 8px; display: block"
+                @change="e => setSelectedKeys(e.target.value ? [e.target.value] : [])"
+                @pressEnter="handleSearch(selectedKeys, confirm, column.dataIndex)"
+            />
+            <a-button
+                type="primary"
+                size="small"
+                style="width: 90px; margin-right: 8px"
+                @click="handleSearch(selectedKeys, confirm, column.dataIndex)"
+            >
+              <template #icon><SearchOutlined /></template>
+              Search
+            </a-button>
+            <a-button size="small" style="width: 90px" @click="handleReset(clearFilters)">
+              Reset
+            </a-button>
+          </div>
+        </template>
+        <template #filterIcon="filtered">
+          <search-outlined :style="{ color: filtered ? '#108ee9' : undefined }" />
+        </template>
+        <template #customRender="{ text, column }">
+      <span v-if="searchText && searchedColumn === column.dataIndex">
+        <template
+            v-for="(fragment, i) in text
+            .toString()
+            .split(new RegExp(`(?<=${searchText})|(?=${searchText})`, 'i'))"
+        >
+          <mark
+              v-if="fragment.toLowerCase() === searchText.toLowerCase()"
+              class="highlight"
+              :key="i"
+          >
+            {{ fragment }}
+          </mark>
+          <template v-else>{{ fragment }}</template>
+        </template>
+      </span>
+          <template v-else>
+            {{ text }}
+          </template>
+        </template>
+      </a-table>
 
     </a-layout-content>
 
@@ -71,18 +116,126 @@
 <script lang="ts">
 import { defineComponent,onMounted,ref,reactive,toRef} from 'vue';
 import axios from'axios';
-import _default from "ant-design-vue/es/vc-trigger/Popup";
+// import _default from "ant-design-vue/es/vc-trigger/Popup"; // 这里是在添加表格组件的时候写的 never use 暂时注释掉
 // import data = _default.data;
+import { SearchOutlined } from '@ant-design/icons-vue';
+
+const data = [
+  {
+    key: '1',
+    name: 'John Brown',
+    age: 32,
+    address: 'New York No. 1 Lake Park',
+  },
+  {
+    key: '2',
+    name: 'Joe Black',
+    age: 42,
+    address: 'London No. 1 Lake Park',
+  },
+  {
+    key: '3',
+    name: 'Jim Green',
+    age: 32,
+    address: 'Sidney No. 1 Lake Park',
+  },
+  {
+    key: '4',
+    name: 'Jim Red',
+    age: 32,
+    address: 'London No. 2 Lake Park',
+  },
+];
 
 export default defineComponent({
   name: 'Home',
   components: {
+    SearchOutlined,
   },
   setup: function () {
     console.log("setup");
     const ebooks=ref();// 响应式数据
     const ebooks1=reactive({books:[]})
     const mains=ref();
+
+    const state = reactive({
+      searchText: '',
+      searchedColumn: '',
+    });
+
+    const searchInput = ref();
+
+    const columns = [
+      {
+        title: 'Name',
+        dataIndex: 'name',
+        key: 'name',
+        slots: {
+          filterDropdown: 'filterDropdown',
+          filterIcon: 'filterIcon',
+          customRender: 'customRender',
+        },
+        onFilter: (value: any, record: any) =>
+            record.name.toString().toLowerCase().includes(value.toLowerCase()),
+        onFilterDropdownVisibleChange: (visible: any) => {
+          if (visible) {
+            setTimeout(() => {
+              console.log(searchInput.value);
+              searchInput.value.focus();
+            }, 0);
+          }
+        },
+      },
+      {
+        title: 'Age',
+        dataIndex: 'age',
+        key: 'age',
+        slots: {
+          filterDropdown: 'filterDropdown',
+          filterIcon: 'filterIcon',
+          customRender: 'customRender',
+        },
+        onFilter: (value: any, record: any) =>
+            record.age.toString().toLowerCase().includes(value.toLowerCase()),
+        onFilterDropdownVisibleChange: (visible: any) => {
+          if (visible) {
+            setTimeout(() => {
+              searchInput.value.focus();
+            });
+          }
+        },
+      },
+      {
+        title: 'Address',
+        dataIndex: 'address',
+        key: 'address',
+        slots: {
+          filterDropdown: 'filterDropdown',
+          filterIcon: 'filterIcon',
+          customRender: 'customRender',
+        },
+        onFilter: (value: any, record: any) =>
+            record.address.toString().toLowerCase().includes(value.toLowerCase()),
+        onFilterDropdownVisibleChange: (visible: any) => {
+          if (visible) {
+            setTimeout(() => {
+              searchInput.value.focus();
+            });
+          }
+        },
+      },
+    ];
+
+    const handleSearch = (selectedKeys: any, confirm: any, dataIndex: any) => {
+      confirm();
+      state.searchText = selectedKeys[0];
+      state.searchedColumn = dataIndex;
+    };
+
+    const handleReset = (clearFilters: any) => {
+      clearFilters();
+      state.searchText = '';
+    };
 
     onMounted(() => {
       console.log("onMounted");
@@ -101,7 +254,22 @@ export default defineComponent({
       ebooks,// html代码要拿到响应式变量，需要在setup最后return
       ebooks2:toRef(ebooks1,"books"),
       mains,
+
+      data,
+      columns,
+      handleSearch,
+      handleReset,
+      searchText: '',
+      searchInput,
+      searchedColumn: '',
     }
   }
 });
 </script>
+
+<style scoped>
+.highlight {
+  background-color: rgb(255, 192, 105);
+  padding: 0px;
+}
+</style>
