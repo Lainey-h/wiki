71041623b36ed9f929000b322b8c7c09b0e1eb20
Lainey-h <houlingling.1@outlook.com>
2021-04-20 16:09:45 +0800

    6-2 增加案例管理页面。 新页面开发：页面、路由、菜单

--- web/src/router/index.ts
+++ web/src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 import Home from '../views/home.vue'
 // import About from '../views/about.vue' //修改的时候conponent报错 所以仍然采用了懒加载
+import AdminMain from '../views/admin/admin-main.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -16,6 +17,11 @@ const routes: Array<RouteRecordRaw> = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
      component: () => import(/* webpackChunkName: "about" */ '../views/about.vue')
+  },
+  {
+    path: '/admin/main',
+    name: 'AdminMain',
+    component: AdminMain
   },
 ]
 
