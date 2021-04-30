b554453516282152e8a238f4ce4681498beab849
Lainey-h <houlingling.1@outlook.com>
2021-04-30 18:19:37 +0800

    9-9 集成redis，登陆成功后将登录信息放入redis中

--- src/main/java/com/lainey/wiki/controller/UserController.java
+++ src/main/java/com/lainey/wiki/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.lainey.wiki.controller;
 
+import com.alibaba.fastjson.JSONObject;
 import com.lainey.wiki.req.UserLoginReq;
 import com.lainey.wiki.req.UserQueryReq;
 import com.lainey.wiki.req.UserResetPasswordReq;
@@ -10,11 +11,15 @@ import com.lainey.wiki.resp.UserLoginResp;
 import com.lainey.wiki.resp.UserQueryResp;
 import com.lainey.wiki.service.UserService;
 import com.lainey.wiki.util.SnowFlake;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.util.DigestUtils;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import javax.validation.Valid;
+import java.util.concurrent.TimeUnit;
 
 @RestController// RestContraller用来返回字符串
 //@Controller//用来返回页面 表示访问地址("/hello")的时候要返回一个页面
@@ -22,9 +27,13 @@ import javax.validation.Valid;
 
 public class UserController {
 
+    private static final Logger LOG = LoggerFactory.getLogger(UserController.class);
 
     @Resource
     private UserService userService;
+
+    @Resource
+    private RedisTemplate redisTemplate;
 
     @Resource
     private SnowFlake snowFlake;
@@ -65,6 +74,13 @@ public class UserController {
         req.setPassword(DigestUtils.md5DigestAsHex(req.getPassword().getBytes())); // 注册的时候（添加用户的时候，密码是经过两层加密的，登陆的时候也应该经过两层加密 这样才能将两层加密后的密文跟数据库进行对比 进行校验）
         CommonResp<UserLoginResp> resp = new CommonResp<>();
         UserLoginResp userLoginResp = userService.login(req);
+
+        Long token = snowFlake.nextId();// 生成token
+        LOG.info("生成单点登录token:{}，并放入ridis中",token);
+        userLoginResp.setToken(token.toString());
+        redisTemplate.opsForValue().set(token.toString(),  JSONObject.toJSONString(userLoginResp), 3600 * 24, TimeUnit.SECONDS);
+
+
         resp.setContent(userLoginResp);
         return resp;
     }
