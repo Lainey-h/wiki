f58e588a024f97a2979eec8ad4dea896c84a1894
Lainey-h <houlingling.1@outlook.com>
2021-05-05 21:59:36 +0800

    9-12 增加路由登录拦截，未登录时跳到首页

--- web/src/router/index.ts
+++ web/src/router/index.ts
@@ -2,6 +2,8 @@ import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 import Home from '../views/home.vue'
 import About from '../views/about.vue' //修改的时候conponent报错 所以仍然采用了懒加载
 import AdminMain from '../views/admin/admin-main.vue'
+import store from "@/store";
+import {Tool} from "@/util/tool";
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -21,12 +23,19 @@ const routes: Array<RouteRecordRaw> = [
   {
     path: '/admin/user',
     name: 'AdminUser',
-    component: () => import(/* webpackChunkName: "about" */ '../views/admin/admin-user.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/admin/admin-user.vue'),
+    meta: {
+      loginRequire: true
+    }
+
   },
   {
     path: '/admin/main',
     name: 'AdminMain',
-    component: () => import(/* webpackChunkName: "about" */ '../views/admin/admin-main.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/admin/admin-main.vue'),
+    meta: {
+      loginRequire: true
+    }
   },
   {
     path: '/more',
@@ -39,5 +48,24 @@ const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
 })
+
+// 路由登录拦截
+router.beforeEach((to, from, next) => {
+  // 要不要对meta.loginRequire属性做监控拦截
+  if (to.matched.some(function (item) {
+    console.log(item, "是否需要登录校验：", item.meta.loginRequire);
+    return item.meta.loginRequire
+  })) {
+    const loginUser = store.state.user;
+    if (Tool.isEmpty(loginUser)) {
+      console.log("用户未登录！");
+      next('/');
+    } else {
+      next();
+    }
+  } else {
+    next();
+  }
+});
 
 export default router
